cc90dfe0daa959298c3774d23ab03f4457abf3cf
mwamba <mkangwa@bluecodeltd.com>
2022-03-01 17:03:17 +0200

    Tasks

--- opensrp-anc/src/main/java/org/smartregister/anc/library/util/ConstantsUtils.java
+++ opensrp-anc/src/main/java/org/smartregister/anc/library/util/ConstantsUtils.java
@@ -125,7 +125,7 @@ public abstract class ConstantsUtils {
         public static final String ANC_TEST = "test";
         public static final String HIV_TEST_AND_RESULTS = "hiv_test";
         public static final String ANC_COUNSELLING_TREATMENT = "anc_counselling_treatment";
-        public static final String ANC_TEST_ENCOUNTER_TYPE = "Tests";
+        public static final String ANC_TEST_ENCOUNTER_TYPE = "Diagnostic Tests and Imaging";
         public static final String ANC_COUNSELLING_TREATMENT_ENCOUNTER_TYPE = "Counselling and Treatment";
         public static final String ANC_SITE_CHARACTERISTICS = "anc_site_characteristics";
         public static final String ANC_QUICK_CHECK = "quick_check";
