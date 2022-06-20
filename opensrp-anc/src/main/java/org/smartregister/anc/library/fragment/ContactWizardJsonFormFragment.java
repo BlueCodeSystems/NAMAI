0bfb4d24d8252d21acce1d8b203d7df46ce69b44
Davidju97 <Davidnyambe9@gmail.com>
2022-06-20 13:24:39 +0200

    Display refferral information on client profile overview

--- opensrp-anc/src/main/java/org/smartregister/anc/library/fragment/ContactWizardJsonFormFragment.java
+++ opensrp-anc/src/main/java/org/smartregister/anc/library/fragment/ContactWizardJsonFormFragment.java
@@ -242,7 +242,7 @@ public class ContactWizardJsonFormFragment extends JsonWizardFormFragment {
                 window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
             }
 
-            yes.setOnClickListener(v -> goToContactFinalize(dialog));
+            yes.setOnClickListener(v -> saveReferral(dialog));
             no.setOnClickListener(v -> quickCheckClose());
 
             dialog.show();
@@ -307,6 +307,13 @@ public class ContactWizardJsonFormFragment extends JsonWizardFormFragment {
         });
     }
 
+    //go to main contact  to auto populate gestation age then display summary
+    public void saveReferral(Dialog dialog)
+    {
+        ((ContactJsonFormActivity) getActivity()).proceedToMainContactPage();
+        goToContactFinalize(dialog);
+
+    }
 
     @Nullable
     private LinearLayout getQuickCheckButtonsLayout() {
